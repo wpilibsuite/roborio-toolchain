1caa39bfe785475924be664122b5c25ba7a28721
Peter Johnson <johnson.peter@gmail.com>
2019-10-11 19:15:16 -0700

    Copy patches folder so patches actually get applied
    This broke the FRC_ROBORIO macro

--- build-windows.sh
+++ build-windows.sh
@@ -3,7 +3,7 @@
 docker pull wpilib/toolchain-builder:18.04 \
  && docker run -v ${PWD}:/artifacts wpilib/toolchain-builder:18.04 bash -c "\
   cp /artifacts/download.sh /artifacts/repack.sh /artifacts/versions.sh . \
-  && cp -R /artifacts/tools /artifacts/linux /artifacts/windows . \
+  && cp -R /artifacts/tools /artifacts/patches /artifacts/linux /artifacts/windows . \
   && zsh download.sh \
   && zsh repack.sh \
   && cd linux \
