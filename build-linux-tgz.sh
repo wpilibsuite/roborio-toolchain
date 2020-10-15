43d08417fe5fa531b70dad502a39be2ef2ad4c82
Ryan Hirasaki <ryanhirasaki@gmail.com>
2020-10-15 14:35:48 -0500

    Bump linux build to 18.04 to match windows

--- build-linux-tgz.sh
+++ build-linux-tgz.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker pull wpilib/toolchain-builder:14.04 \
- && docker run -v ${PWD}:/artifacts wpilib/toolchain-builder:14.04 bash -c "\
+docker pull wpilib/toolchain-builder:18.04 \
+ && docker run -v ${PWD}:/artifacts wpilib/toolchain-builder:18.04 bash -c "\
   cp /artifacts/download.sh /artifacts/repack.sh /artifacts/versions.sh . \
   && cp -R /artifacts/tools /artifacts/patches /artifacts/linux . \
   && echo 'downloading' && zsh download.sh \
