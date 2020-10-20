89a7595187b2700c76d36f360a860187d6307af7
Ryan Hirasaki <ryanhirasaki@gmail.com>
2020-10-20 17:45:20 -0500

    GPG initalize error catch

--- download.sh
+++ download.sh
@@ -15,8 +15,7 @@ PUB_KEYS=(
 for KEY in "${PUB_KEYS[@]}"
 do
     curl -sSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x${KEY}" | \
-        gpg --import
-    # gpg --keyserver keyserver.ubuntu.com --recv-keys "$KEY"
+        gpg --import || { echo "Could not import 0x${KEY} into gpg"; exit 1; }
 done
 
 signed sig https://ftp.gnu.org/gnu/gcc/gcc-${V_GCC}/gcc-${V_GCC}.tar.gz
